182effdadae1df167404eb5b669c8329c290cd2e
Alexander Ertli <ertli.alexander@googlemail.com>
2025-09-13 11:13:32 +0200

    Bump version: 0.2.6 → 0.3.0

--- echo/config.py
+++ echo/config.py
@@ -16,7 +16,7 @@ class Settings(BaseSettings):
     # Application info
     APP_NAME: str = "Echo Tool Server"
     LOG_LEVEL: str = "WARN"
-    APP_VERSION: str = "0.2.6"
+    APP_VERSION: str = "0.3.0"
 
     # Contact info
     CONTACT_NAME: str = "API Support"
